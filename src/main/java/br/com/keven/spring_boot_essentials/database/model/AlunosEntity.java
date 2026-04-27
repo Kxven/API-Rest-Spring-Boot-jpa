e994a66216d939c3e74d594761dea5d3010c1b47
kevenn <kevengodinho3474@gmail.com>
2026-04-27 12:22:31 -0300

    exerciciosEntity and TreinosEntitity and relationshiops

--- src/main/java/br/com/keven/spring_boot_essentials/database/model/AlunosEntity.java
+++ src/main/java/br/com/keven/spring_boot_essentials/database/model/AlunosEntity.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="alunos")
@@ -27,5 +30,8 @@ public class AlunosEntity {
     @OneToOne
     @JoinColumn(name = "avaliacao_fisica_id")
     private AvaliacoesFisicasEntity avaliacoesFisicas;
+
+    @OneToMany(mappedBy = "aluno")
+    private Set<TreinosEntity> treino = new HashSet<>();
 
 }
